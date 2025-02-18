2ce7e658d18506f6e6c182c61d328fe2aebb9db2
ikerfuot <166533065+ikerfuot@users.noreply.github.com>
2025-02-18 18:17:24 +0100

    Update main.java

--- Bomberman/src/main.java
+++ Bomberman/src/main.java
@@ -2,7 +2,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.EmptyBorder; //xd
 
 public class main extends JFrame {
 
